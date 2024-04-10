5f335a4fc76cd3e8174eeb8002bbc61c92593a90
sladey01 <duncan.slade@snyk.io>
2024-04-10 10:44:41 +0100

    code test

--- typeorm-db.js
+++ typeorm-db.js
@@ -9,7 +9,7 @@ typeorm.createConnection({
   host: "localhost",
   port: 3306,
   username: "root",
-  password: "root",
+  password: env.variable,
   database: "acme",
   synchronize: true,
   "logging": true,
